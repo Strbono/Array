bf4ad4fdc52e0b85b72e7638be1281e551381a18
satrio <wibicrull35@gmail.com>
2021-06-29 19:48:51 +0700

    task and exercise

---
+++ LatAR/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatAR
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arrNama = { "erick", "wely", "sony" };
+
+            Console.WriteLine(arrNama[2].ToString() + "\n");
+
+            for (int i = 0; i < arrNama.Length; i++)
+            {
+                Console.WriteLine(arrNama[i]);
+            }
+            Console.WriteLine();
+
+            foreach (string nama in arrNama)
+            {
+                Console.WriteLine(nama);
+            }
+            Console.WriteLine();
+
+            int[] arrBil = new int[3];
+
+            arrBil[0] = 75;
+            arrBil[1] = 80;
+            arrBil[2] = 95;
+
+
+            foreach (int bil in arrBil)
+            {
+                Console.WriteLine(bil.ToString());
+            }
+
+
+            for (int i = 0; i < arrNama.Length; i++)
+            {
+                Console.WriteLine(arrNama[i], arrBil);
+            }
+
+            Console.WriteLine();
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ TugArr/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TugArr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mahasiswa mhs1 = new Mahasiswa();
+            mhs1.NIM = 1234;
+            mhs1.Nama = "Erik";
+            mhs1.Nilai = 75;
+
+            Mahasiswa mhs2 = new Mahasiswa();
+            mhs2.NIM = 1235;
+            mhs2.Nama = "wely";
+            mhs2.Nilai = 80;
+
+            Mahasiswa mhs3 = new Mahasiswa();
+            mhs3.NIM = 1235;
+            mhs3.Nama = "sony";
+            mhs3.Nilai = 95;
+
+            Mahasiswa[] armhs = new Mahasiswa[3];
+
+            armhs[0] = mhs1;
+            armhs[1] = mhs2;
+            armhs[2] = mhs3;
+
+
+            Console.WriteLine("No. NIM  Nama  Nilai");
+            Console.WriteLine("---------------------------");
+
+            int no = 1;
+
+            foreach (Mahasiswa mhs in armhs)
+            {
+                Console.WriteLine($"{no++} {mhs.NIM} {mhs.Nama} {mhs.Nama} {mhs.Nilai} ");
+            }
+
+            Console.ReadKey();
+
+
+
+            /*Console.WriteLine("No. NIM  Nama  Nilai");
+            Console.WriteLine("---------------------------"); 
+            Console.WriteLine("1. {0} {1} {2}", mhs1.NIM, mhs1.Nama, mhs1.Nilai);
+            Console.WriteLine("1. {0} {1} {2}", mhs2.NIM, mhs2.Nama, mhs2.Nilai);
+            Console.WriteLine("1. {0} {1} {2}", mhs3.NIM, mhs3.Nama, mhs3.Nilai);
+            */
+        }
+    }
+}
